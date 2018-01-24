e8c2face224f16ab2d2fa927a0c944176e4dd557
“Bai <xb1994816@gmail.com>
2018-01-23 22:02:20 -0500

    fixed main.py and app.yaml

--- appengine/standard/flask/tutorial/main.py
+++ appengine/standard/flask/tutorial/main.py
@@ -12,8 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+
 # [START app]
 import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
 
 # [START imports]
 from flask import Flask, render_template, request
@@ -30,6 +43,10 @@ def form():
     return render_template('form.html')
 # [END form]
 
+@app.route('/')
+def hello():
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
 
 # [START submitted]
 @app.route('/submitted', methods=['POST'])
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 """`main` is the top level module for your Flask application."""
-
 # Imports
 import os
 import jinja2
 import webapp2
 import logging
 
+# [START app]
+import logging
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+
+
+# [START imports]
+from flask import Flask, render_template, request
     
+# Import the Flask Framework
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
     return template.render()
+ 
